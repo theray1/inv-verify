ee751d7e6480448bb14db44d254f10270daca81e
Erwan BOISTEAU-DESDEVISES <E196630D@nfsrootha0.ensinfo.sciences.univ-nantes.prive>
2023-10-20 16:52:12 +0200

    continued impl

--- LogicalProposition.py
+++ LogicalProposition.py
@@ -1,5 +1,19 @@
+from Node import Node
+
 class LogicalProposition:
 
+    def and_operator(self, lp: LogicalProposition):
+        return 
 
-    def __init__(self):
+    def __init__(self, ):
+    
+    class LogicalAnd:
         pass
+    
+    class LogicalOr:
+        pass
+
+    class LogicalNot:
+        pass
+
+    class
--- TransitionSystem.py
+++ TransitionSystem.py
@@ -4,11 +4,14 @@ class TransitionSystem:
     def get_initial_nodes(self):
         return self.initial_nodes
 
+    def add_initial_node(self, node: Node):
+        self.initial_nodes.append(node)
+
     def __init__(self):
         pass
         
 class Node:
-    label = ""
+    labels = []
     next_nodes = []
     
     def get_next_nodes(self):
